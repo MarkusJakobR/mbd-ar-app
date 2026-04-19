383ce28fae947a5f1ec916988a12bccf34da4d94
MarkusJakobR <mcrecaplaza@up.edu.ph>
2026-04-19 23:24:11 +0800

    feat: upgraded loading box in ar mode

--- lib/widgets/ar/ar_loading_box.dart
+++ lib/widgets/ar/ar_loading_box.dart
@@ -1,19 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'dart:math';
 
-class ARLoadingBox extends StatelessWidget {
+class ARLoadingBox extends StatefulWidget {
   const ARLoadingBox({super.key});
+
+  @override
+  State<ARLoadingBox> createState() => _ARLoadingBoxState();
+}
+
+class _ARLoadingBoxState extends State<ARLoadingBox>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  final String _text = 'Loading AR...';
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black54,
-      child: const Center(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircularProgressIndicator(color: Colors.white),
-            SizedBox(height: 12),
-            Text('Loading AR...', style: TextStyle(color: Colors.white)),
+            Lottie.asset(
+              'assets/videos/loading.json',
+              width: 200,
+              height: 200,
+              fit: BoxFit.contain,
+            ),
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, _) {
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: List.generate(_text.length, (index) {
+                    // Each letter has a wave offset based on index
+                    final wave = sin(
+                      (_controller.value * 2 * pi) - (index * 0.4),
+                    );
+                    final offset = wave * 4; // 4px wave height, adjust to taste
+
+                    return Transform.translate(
+                      offset: Offset(0, offset),
+                      child: Text(
+                        _text[index],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                    );
+                  }),
+                );
+              },
+            ),
           ],
         ),
       ),
